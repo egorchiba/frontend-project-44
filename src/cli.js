d53183bb2d435f26b702cc0f953a911dcf90cfa5
Egor Chebanov <egorchiba@gmail.com>
2023-04-11 00:57:40 +0300

    add cli.js

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+
+const whatsYourName = () => {
+    console.log('Welcome to Brain Games!');
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
+};
+
+export {whatsYourName};
