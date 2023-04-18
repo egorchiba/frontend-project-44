8e1019efbca6342d0e18d7d811f6885556c8c851
Egor Chebanov <egorchiba@gmail.com>
2023-04-18 18:26:12 +0300

    fix brain-games

--- src/welcome.js
+++ src/welcome.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const whatsYourName = () => {
-  console.log('Welcome to Brain Games!');
+  console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 
